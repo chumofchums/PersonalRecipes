3de38f272fe1c8041461f993f266bbad3547a5c9
chumofchums <106637416+chumofchums@users.noreply.github.com>
2023-02-16 14:17:12 -0800

    Add CapsuleCast
    Add CapsuleCast to Player script

--- Player.cs
+++ Player.cs
@@ -5,12 +5,25 @@ public class Player : MonoBehaviour
 
     [SerializeField] private GameInput gameInput;
     [SerializeField] private float moveSpeed = 5f;
+
+    private bool isWalking;
     private void Update()
     {
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
 
         Vector3 moveDirection = new Vector3(inputVector.x, 0f, inputVector.y);
-        transform.position += moveDirection * moveSpeed * Time.deltaTime;
+
+        float moveDistance = moveSpeed * Time.deltaTime;
+        float playerRadius = .3f;
+        float playerHeight = 1f;
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirection, moveDistance);
+
+        if (canMove)
+        {
+            transform.position += moveDirection * moveDistance;
+        }
+
+        isWalking = moveDirection != Vector3.zero;
 
         float rotateSpeed = 10f;
         transform.forward = Vector3.Slerp(transform.forward, moveDirection, Time.deltaTime * rotateSpeed);
