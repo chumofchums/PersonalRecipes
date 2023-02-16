58c51acc4c8b60d1bc6210b223399ef1232ae1c4
chumofchums <106637416+chumofchums@users.noreply.github.com>
2023-02-16 13:02:56 -0800

    Player script refactored to use new Input System

--- Player.cs
+++ Player.cs
@@ -2,35 +2,17 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-  [SerializeField] private float moveSpeed = 7f;
-  private void Update()
-  {
-    Vector2 inputVector = new Vector2(0, 0);
 
-    if (Input.GetKey(KeyCode.W))
+    [SerializeField] private GameInput gameInput;
+    [SerializeField] private float moveSpeed = 5f;
+    private void Update()
     {
-      inputVector.y = +1;
-    }
-    if (Input.GetKey(KeyCode.S))
-    {
-      inputVector.y = -1;
-    }
-    if (Input.GetKey(KeyCode.D))
-    {
-      inputVector.x = +1;
-    }
-    if (Input.GetKey(KeyCode.A))
-    {
-      inputVector.x = -1;
-    }
-    inputVector = inputVector.normalized;
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
 
-    Vector3 moveDirection = new Vector3(inputVector.x, 0f, inputVector.y);
-    transform.position += moveDirection * moveSpeed * Time.deltaTime;
+        Vector3 moveDirection = new Vector3(inputVector.x, 0f, inputVector.y);
+        transform.position += moveDirection * moveSpeed * Time.deltaTime;
 
-    float rotateSpeed = 10f;
-    transform.forward = Vector3.Slerp(transform.forward, moveDirection, Time.deltaTime * rotateSpeed);
-  }
+        float rotateSpeed = 10f;
+        transform.forward = Vector3.Slerp(transform.forward, moveDirection, Time.deltaTime * rotateSpeed);
+    }
 }
-
-
